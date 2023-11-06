ea4ce2a6af198a6189f848d5f01ce1fa0ff62ea2
Brant Cao <bc000151@sd38.bc.ca>
2023-11-06 09:49:50 -0800

    working on mcdonalds

---
+++ mcdonalds.py
@@ -0,0 +1,21 @@
+
+print ("Would you like to buy a burger for $5? (Yes/No)")
+    if input: "yes"
+
+
+
+
+
+    "Would you like to buy fries for $3? (Yes/No)",
+    "Would you like to buy a soda for $1.5? (Yes/No)"
+    ]
+
+# Ask the user the questions
+
+for question in questions:
+    print(question)
+
+if input: "yes"
+
+
+    # Store their responses
---
+++ test.py
@@ -0,0 +1,4 @@
+x = "The number is: "
+y = 5
+x += y  
+print(x)
