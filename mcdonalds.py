d42fd47af454c23bf3af5eed265dc1c68d5e527a
Brant Cao <bc000151@sd38.bc.ca>
2023-11-07 14:57:50 -0800

    mcdolands update

--- mcdonalds.py
+++ mcdonalds.py
@@ -1,21 +1,36 @@
 
-print ("Would you like to buy a burger for $5? (Yes/No)")
-    if input: "yes"
+# Chatbot
+# Author: Brant
+# Date: 7 November 2023
 
+# Greet the user
+print("Hello welcome to Mcdonald's")
 
+# Ask the user
+burger = input("Would you like to buy a burger for $5? (Yes/No)").lower()
+fries = input("Would you like to buy fries for $3? (Yes/No)").lower()
+soda = input ("Would you like to buy a soda for $1.5? (Yes/No)").lower()
 
+# tore their answers
+total_price = 0
 
+if burger == "yes":
+    total_price += 5
 
-    "Would you like to buy fries for $3? (Yes/No)",
-    "Would you like to buy a soda for $1.5? (Yes/No)"
-    ]
+if fries == "yes":
+    total_price += 3
 
-# Ask the user the questions
+if soda == "yes":
+    total_price += 1.5
 
-for question in questions:
-    print(question)
+# Calculations
 
-if input: "yes"
+print("Total price:", total_price)
 
+# With tax
+tax_rate = 0.14
+total_with_tax = total_price * (1 + tax_rate)
 
-    # Store their responses
+# Now, round the total including tax to 2 decimal places and print it
+rounded_total_with_tax = round(total_with_tax, 2)
+print("Your total with tax comes to: $", rounded_total_with_tax)
